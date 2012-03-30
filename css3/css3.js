60633a973e4344f528758df573a0e8dea187e4f4
Trent Kocurek <t2kocurek@gmail.com>
2012-03-29 23:10:05 -0500

    added ability to toggle menu styles

--- css3/css3.js
+++ css3/css3.js
@@ -1,10 +1,14 @@
 $(function() {
-	$('a').on('click', function() {
+	$('a:not(#menu-styler)').on('click', function() {
 		$('#container').load($(this).attr('href'));	return false;
 	});
 
 	$('div').on('click', 'div.example-container div', function() {
 		var $this = $(this);
 		$this.toggleClass($this.attr('id'));
+	});
+
+	$('a#menu-styler').on('click', function(e) {
+		$('a').toggleClass('styled'); return false;
 	});
 });
